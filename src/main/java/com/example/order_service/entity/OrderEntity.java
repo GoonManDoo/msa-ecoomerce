e9d4383e7e3e9b30563e32b060302d5de3277f10
PC <kym8439@gmail.com>
2024-08-18 20:52:59 +0900

    order-service

---
+++ src/main/java/com/example/order_service/entity/OrderEntity.java
@@ -0,0 +1,45 @@
+package com.example.order_service.entity;
+
+import lombok.Data;
+
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+@Data
+@Entity
+@Table(name = "orders")
+public class OrderEntity implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 120, unique = true)
+    private String productId;
+
+    @Column(nullable = false)
+    private Integer qty;
+
+    @Column(nullable = false)
+    private Integer unitPrice;
+
+    @Column(nullable = false)
+    private Integer totalPrice;
+
+    @Column(nullable = false)
+    private String userId;
+
+    @Column(nullable = false, unique = true)
+    private String orderId;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Date createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        if (createdAt == null) {
+            createdAt = new Date();
+        }
+    }
+}
